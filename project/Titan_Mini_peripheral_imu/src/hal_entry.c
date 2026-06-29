f3386aa61acb212f93c0b3bc2099bfb9fb2b17e4
hydevcode <zhjich312@126.com>
2026-06-29 18:38:20 +0800

    SDK1.2.0更新

--- project/Titan_Mini_peripheral_imu/src/hal_entry.c
+++ project/Titan_Mini_peripheral_imu/src/hal_entry.c
@@ -12,26 +12,18 @@
 #include "hal_data.h"
 #include <rtdevice.h>
 #include <board.h>
-#include "lsm6ds3tr-c_reg.h"
 
-#define DBG_TAG     "led"
+#define DBG_TAG     "imu"
 #define DBG_LVL     DBG_LOG
 #include <rtdbg.h>
 
-/* I2C 配置 */
-#define LSM6DS3TR_C_I2C_BUS    "i2c2"
-#define LSM6DS3TR_C_I2C_ADDR    0x6A  /* SA0引脚接GND */
-
-/* 配置 LED 灯引脚 */
 #define LED_PIN_R   BSP_IO_PORT_01_PIN_09
 #define LED_PIN_B   BSP_IO_PORT_01_PIN_10
 #define LED_PIN_G   BSP_IO_PORT_01_PIN_08
 
-/* 定义 LED 亮灭电平 */
 #define LED_ON  (0)
 #define LED_OFF (1)
 
-/* 定义 8 组 LED 闪灯表，其顺序为 R B G */
 static const rt_uint8_t _blink_tab[][3] =
 {
     {LED_OFF, LED_OFF, LED_OFF},
@@ -48,14 +40,18 @@ void hal_entry(void)
 {
     rt_kprintf("\nHello RT-Thread!\n");
     rt_kprintf("==================================================\n");
-    rt_kprintf("This example project is an IMU routine!\n");
+    rt_kprintf("Titan Mini peripheral IMU demo\n");
+    rt_kprintf("  - LED RGB blink runs on boot\n");
+    rt_kprintf("  - LSM6DS3TR-C IMU auto-init on boot, commands:\n");
+    rt_kprintf("      imu                 read & print one sample\n");
+    rt_kprintf("      imu start [ms]      start periodic sampling\n");
+    rt_kprintf("      imu stop            stop periodic sampling\n");
     rt_kprintf("==================================================\n");
 
     unsigned int count = 0;
     unsigned int group_num = sizeof(_blink_tab)/sizeof(_blink_tab[0]);
     unsigned int group_current;
 
-    /* 设置 RGB 灯引脚为输出模式 */
     rt_pin_mode(LED_PIN_R, PIN_MODE_OUTPUT);
     rt_pin_mode(LED_PIN_G, PIN_MODE_OUTPUT);
     rt_pin_mode(LED_PIN_B, PIN_MODE_OUTPUT);
@@ -66,10 +62,13 @@ void hal_entry(void)
     do
     {
         group_current = count % group_num;
+
         rt_pin_write(LED_PIN_R, _blink_tab[group_current][0]);
         rt_pin_write(LED_PIN_B, _blink_tab[group_current][1]);
         rt_pin_write(LED_PIN_G, _blink_tab[group_current][2]);
+
         count++;
+
         rt_thread_mdelay(500);
     }while(count > 0);
 }
